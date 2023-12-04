adb0ee35ead643c717446fae66c490288204bfff
vaishnavirshah <vaishnavirahulshah@gmail.com>
2023-12-04 00:51:59 -0500

    adding db & api related files

---
+++ pts/src/test/java/com/example/patient_tracker_system/PatientTrackerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patient_tracker_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientTrackerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
