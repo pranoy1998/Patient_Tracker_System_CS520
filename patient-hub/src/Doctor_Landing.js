4b2ee831d7ca0aa5b92968bc79c5d404d720918a
pranoydev98 <pranoydev98@gmail.com>
2023-12-08 20:00:01 -0500

    Patient and Doctor Landing UI structure_v1

--- patient-hub/src/Doctor_Landing.js
+++ patient-hub/src/Doctor_Landing.js
@@ -1,18 +1,60 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { useHistory ,useNavigate, useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from 'cdbreact';
+import { NavLink } from 'react-router-dom';
+import './Patient_Landing.css'
 
 const DoctorLanding = ({}) => {
   
   const location = useLocation();
   const id = location.state.id;
-
   //alert(id);
 
   return (
-    <div>
-      <h1>Hello Doctor</h1>
+    <div class="main">
+
+<nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse">
+    {/* <div class="position-sticky"> */}
+      <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
+          <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Daily Schedule</span>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
+          <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Appointments</span>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
+          <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Patients</span>
+        </a>
+        <a class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
+          <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Logout</span>
+        </a>
+      </div>
+  </nav>
+
+    <nav id="main-navbar" class="navbar navbar-expand-lg navbar-light fixed-top">
+      <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#sidebarMenu"
+        aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+      <h1 class="navbar-brand">
+        Doctor Portal
+      </h1>
+    </nav>
+
+    <div class="maincontent">
+    <h1>Component Loading........</h1>
     </div>
+
+  </div>
   );
 };
 
--- patient-hub/src/Patient_Landing.js
+++ patient-hub/src/Patient_Landing.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { useHistory ,useNavigate, useLocation } from 'react-router-dom';
+import SidebarMenu from 'react-bootstrap-sidebar-menu';
+import './Patient_Landing.css'
 
 const PatientLanding = ({}) => {
 
@@ -10,10 +12,38 @@ const PatientLanding = ({}) => {
   //alert(id);
 
   return (
-    <div>
-      <h1>Hello Patient</h1>
+    <div class="main">
+
+<nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse">
+    {/* <div class="position-sticky"> */}
+      <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
+          <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>My Info</span>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
+          <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Appointments</span>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
+          <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Logout</span>
+        </a>
+      </div>
+  </nav>
+
+    <nav id="main-navbar" class="navbar navbar-expand-lg navbar-light fixed-top">
+      <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#sidebarMenu"
+        aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+      <h1 class="navbar-brand">
+        Patient Portal
+      </h1>
+    </nav>
+
+    <div class="maincontent">
+    <h1>Component Loading........</h1>
     </div>
+
+  </div>
   );
 };
-
 export default PatientLanding;
