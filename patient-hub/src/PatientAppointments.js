8a1f99f5449c4dd09c5f6df28cf8413c8cfd32d4
pranoydev98 <pranoydev98@gmail.com>
2023-12-12 14:51:52 -0500

    Patient Appointment List Setup

--- patient-hub/src/PatientAppointments.js
+++ patient-hub/src/PatientAppointments.js
@@ -1,14 +1,123 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory ,useNavigate, useLocation } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './PatientAppointments.css'
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
-const PatientAppointments = (props) => {
+function PatientAppointments(props){
 
     const id = props.id;
 
-    return (
-        <div class="main">
-            <h1>Appointments Page for patient : {props.id} </h1>
-        </div>
-    );
-  };
-  
-  export default PatientAppointments;
+    function DayTask({inputTime,inputDoctorName}){
+        const [time, setTime] = useState(inputTime);
+        const [DoctorName, setPatientName] = useState(inputDoctorName);
+    
+        return(
+            <div className="day-task-rounded-rectangle" style={{ height: '60px' , backgroundColor : 'white' , width: '45%', position: 'relative' ,left:"28%"}}>
+                <p className='PatientTimeSlot'><b>Time Slot</b> : {time}</p> <p className='DoctorName'><b>Doctor Name</b> : {DoctorName}</p>
+            </div>
+        );
+    }
+    
+    function DateStamp({initDate}){
+        // CHANGE IT TO DATE LATER
+        const [date, setDate] = useState(initDate);
+    
+        return(
+            <div className="container">
+                 <div >
+                 <p>{date.toString()}</p>
+                </div>
+            </div>
+           
+        );
+    }
+    
+    function Day({inputDate,inputTasks}){
+        const [date, setDate] = useState(inputDate);
+        const [tasks, setItem] = useState(inputTasks);
+    
+        return(
+            <div style={{ height: '20%' }}>
+                <DateStamp initDate={date}/>
+                {tasks.map(item => (
+                <ul>
+                    <DayTask inputTime={item.time} inputDoctorName={item.doctorName}/>
+                </ul>
+                ))}
+            </div>
+        );
+    }
+
+  const sampleTaskList= 
+    [
+        {
+            id: "Tuesday : 11/12/2023",
+            value:
+            [{
+                time:"9AM - 10AM",
+                doctorName:"A",
+            },
+            {
+                time:"12PM - 1PM",
+                doctorName:"Pranoy Pranoy Dev Dev",
+            },
+            {
+                time:"1PM - 2PM",
+                doctorName:"PD",
+            },
+        ]
+        },
+        {
+            id: "Thursday : 13/12/2023",//new Date(2011,11,2).toString(),
+            value:
+            [{
+                time:"9AM - 10AM",
+                doctorName:"A'",
+            },
+            {
+                time:"1PM - 2PM",
+                doctorName:"B'",
+            }]
+        },
+        {
+            id: "Thursday : 13/12/2023",//new Date(2011,11,2).toString(),
+            value:
+            [{
+                time:"9AM - 10AM",
+                doctorName:"A'",
+            },
+            {
+                time:"1PM - 2PM",
+                doctorName:"B'",
+            }]
+        },
+        {
+            id: "Thursday : 13/12/2023",//new Date(2011,11,2).toString(),
+            value:
+            [{
+                time:"9AM - 10AM",
+                doctorName:"A'",
+            },
+            {
+                time:"1PM - 2PM",
+                doctorName:"B'",
+            }]
+        }
+    ];
+
+
+  const taskList = sampleTaskList;
+
+  return (
+    <div className="daily-schedule-container">
+        {taskList.map(item => (
+            <React.Fragment key={item.id}>
+                <Day inputDate={item.id} inputTasks={item.value} />
+            </React.Fragment>
+        ))}      
+    </div>
+  );
+}
+
+export default PatientAppointments;
