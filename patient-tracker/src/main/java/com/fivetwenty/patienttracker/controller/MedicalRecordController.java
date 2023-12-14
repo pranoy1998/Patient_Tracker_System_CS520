7713a5cf0c9e33f93a9a142278a3401466beffff
vaishnavirshah <vaishnavirahulshah@gmail.com>
2023-12-14 16:04:03 -0500

    added upload docs api

--- patient-tracker/src/main/java/com/fivetwenty/patienttracker/controller/MedicalRecordController.java
+++ patient-tracker/src/main/java/com/fivetwenty/patienttracker/controller/MedicalRecordController.java
@@ -1,15 +1,30 @@
 package com.fivetwenty.patienttracker.controller;
 
+import com.fivetwenty.patienttracker.model.MedicalRecord;
 import com.fivetwenty.patienttracker.repository.MedicalRecordRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MedicalRecordController {
 
     private final MedicalRecordRepository medicalRecordRepository;
-
     public MedicalRecordController(MedicalRecordRepository medicalRecordRepository) {
         this.medicalRecordRepository = medicalRecordRepository;
+    }
+    
+    @GetMapping("/patient/{patientId}/uploaded-documents")
+    public ResponseEntity<List<MedicalRecord>> getUploadedDocumentsByPatientId(@PathVariable int patientId) {
+        List<MedicalRecord> medicalRecords = medicalRecordService.getMedicalRecordById(patientId);
+
+        List<byte[]> uploadedDocuments = medicalRecords.stream()
+                .map(MedicalRecord::getUploadeddocs)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(uploadedDocuments);
     }
 }
 
