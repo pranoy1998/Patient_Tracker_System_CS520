ca322a8de52590c96539e2ad0b58e8618711d5d7
Yash Kothekar <yashkothekar1900@gmail.com>
2023-12-14 23:56:54 -0500

    Patient info changes

--- patient-hub/src/PatientInfo.js
+++ patient-hub/src/PatientInfo.js
@@ -7,31 +7,3 @@ import {
   MDBBtn,
   MDBIcon
 } from 'mdb-react-ui-kit';
-
-export default function PatientInfo(props){
-
-  return(
-    <div>
-    <h1>Here at Patients Info Page for patientid : {props.id}</h1>
-    </div>
-  );
-  }
-  // return (
-  //   <form>
-  //     <MDBRow className='mb-4'>
-  //       <MDBCol>
-  //       <MDBInput label='Example label' id='form1' type='text' />
-  //       </MDBCol>
-  //       <MDBCol>
-  //         <MDBInput id='form3Example2' label='Last name' />
-  //       </MDBCol>
-  //     </MDBRow>
-  //     <MDBInput className='mb-4' type='email' id='form3Example3' label='Email address' />
-  //     <MDBInput className='mb-4' type='password' id='form3Example4' label='Password' />
-
-  //     <MDBBtn type='submit' className='mb-4' block>
-  //       Save
-  //     </MDBBtn>
-  //   </form>
-  // );
-// }
