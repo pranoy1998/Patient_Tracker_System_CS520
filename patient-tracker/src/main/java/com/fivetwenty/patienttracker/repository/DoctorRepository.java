885cd714031693576340e037e7da3ab44dce52c2
mshishodia123 <134175809+mshishodia123@users.noreply.github.com>
2023-12-15 22:41:54 -0500

    Delete all incorrect code

--- patient-tracker/src/main/java/com/fivetwenty/patienttracker/repository/DoctorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.fivetwenty.patienttracker.repository;
-
-import com.fivetwenty.patienttracker.model.Doctor;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DoctorRepository extends CrudRepository<Doctor, Integer> {}
