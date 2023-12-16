bed187e5b4072b65d8281233672865c3529b3127
mshishodia123 <134175809+mshishodia123@users.noreply.github.com>
2023-12-15 22:46:23 -0500

    Delete patient-tracker/src directory
    Replacing with correct APIs

--- patient-tracker/src/java/com/fivetwenty/patienttracker/repository/MedicalRecordRepository.java
+++
@@ -1,6 +0,0 @@
-package com.fivetwenty.patienttracker.repository;
-
-import com.fivetwenty.patienttracker.model.MedicalRecord;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MedicalRecordRepository extends CrudRepository<MedicalRecord, Integer> {}
--- patient-tracker/src/java/com/fivetwenty/patienttracker/repository/PatientRepository.java
+++
@@ -1,6 +0,0 @@
-package com.fivetwenty.patienttracker.repository;
-
-import com.fivetwenty.patienttracker.model.Patient;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PatientRepository extends CrudRepository<Patient, Integer> {}
