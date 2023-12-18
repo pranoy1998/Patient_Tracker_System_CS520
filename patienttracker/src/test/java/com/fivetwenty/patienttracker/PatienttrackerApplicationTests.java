9904211aa6b14b2e8b2895e04020ae477942e58b
MeghaShishodia <megha.shishodia1@gmail.com>
2023-12-17 21:08:39 -0500

    Removing corrupt code

---
+++ patienttracker/src/test/java/com/fivetwenty/patienttracker/PatienttrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivetwenty.patienttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatienttrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
